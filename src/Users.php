d81eaf50e5b42fba64c09e6feb8ef9d9a314e662
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-02 15:59:21 +0200

    added connection.php and modif User.php

--- src/Users.php
+++ src/Users.php
@@ -22,7 +22,6 @@ class Users{
     static public function  setConnection (mysqli $newConnection){
         self::$conn = $newConnection;
     }
-
     static public function login($email, $password){
         $sql = "SELECT * FROM Users WHERE email='$email'";
         $result = self::$conn->query($sql);
@@ -38,8 +37,6 @@ class Users{
             }
         }
     }
-
-
     static public function  register($newEmail, $password, $password2, $newDescription){
         if ($password != $password2){
             return false;
@@ -48,7 +45,7 @@ class Users{
         $hassedPassword = password_hash($password, PASSWORD_BCRYPT);
 
         $sql = "INSERT INTO Users (email, password, description)
-        VALUES ('$newEmail', '$hassedPassword', '$description')";
+        VALUES ('$newEmail', '$hassedPassword', '$newDescription')";
         $result = self::$conn->query($sql);
         if($result == true){
             $newUser = new Users(self::$conn->insert_id, $newEmail, $newDescription);
@@ -56,7 +53,31 @@ class Users{
         }
         return false;
     }
-
+    static public function getUserById($id){
+        $sql = "SELECT * FROM Users WHERE user_id ={$id}";
+        $return = self::$conn->query($sql);
+        if($return == true){
+            if($return->num_rows == 1){
+                $row = $return->fetch_assoc();
+                $loggedUser = new Users($row["user_id"], $row["email"], $row["description"] );
+                return$loggedUser;
+            }
+        }
+    }
+    static public function getAllUsers(){
+        $ret = [];
+        $sql = "SELECT * FROM Users ";
+        $result = self::$conn->query($sql);
+        if ($result == true){
+            if($result->num_rows > 0 ){
+                while ($row = $result->fetch_assoc()){
+                    $loadedUser = new Users($row["user_id"], $row["email"], $row["description"]);
+                    $ret[] = $loadedUser;
+                }
+            }
+        }
+        return $ret;
+    }
     public function __construct($newId, $newEmail, $newDescription){
         $this->id =$newId;
         $this->email = $newEmail;
