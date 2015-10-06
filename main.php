9344b6f368e6c69dd6ec31e6d5d96de870e4469e
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-06 09:12:33 +0200

    added main.php with link to create messages and echo to Tweets

--- main.php
+++ main.php
@@ -15,13 +15,18 @@ echo ("Witaj " . $myUser->getEmail());
 <form action="main.php" method="POST" >
     <input type="text" name="tweet" placeholder="dodaj tweet">
     <input type="submit" value="send">
+    <a href='messagesWindow.php?user_id=<?php echo $myUser->getId(); ?>'>create message</a>
 </form>
 
 <?php
 echo "<a href='show_user.php?user_id={$myUser->getId()}'>User Profile</a><br>";
 $allTweets = Tweet::loadAllTweets();
+//$allUsers = Users::getUserById();
 foreach ($allTweets as $tweet){
-    echo "Tweet:<br>";
+    //echo "Tweet by: ".Users::getUsersById($tweet->getUserId()). "<br>"; // nie dziala
+    $userId = $tweet->getUserId();
+    $allUsers = Users::getUserById($userId);
+    echo "<br>Tweet by user name: {$allUsers->getUserName()}<br>";
     echo "Text: {$tweet->getText()}<br>";
     echo "Creation Date: {$tweet->getCreationDate()}<br>";
     echo "<a href='show_tweet.php?tweet_id={$tweet->getId()}'>SHOW Tweet </a><br> ";
