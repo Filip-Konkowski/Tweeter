dc31c7e63964615c75f9077097de8f042b868596
Filip-Konkowski <fkonkowski@gmail.com>
2015-10-06 14:23:19 +0200

    add connection.php

--- src/connection.php
+++ src/connection.php
@@ -1,15 +1,14 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: filip
- * Date: 02.10.15
- * Time: 11:12
- */
+
 require_once("Users.php");
+require_once("tweet.php");
+require_once("comment.php");
+require_once("message.php");
+
 $userName = "";
-$host = "";
+$host = "localhost";
 $pass = "";
-$dbName = "";
+$dbName = "tweeter";
 
 $conn = new mysqli($host, $userName, $pass, $dbName);
 
@@ -18,9 +17,47 @@ if ($conn == false){
 }
 
 Users::setConnection($conn);
-//$createUser = Users::register("nowy@goo.pl", "haslo1", "haslo1", "Taki zapis");
+Tweet::setConnection($conn);
+Comment::setConnection($conn);
+Message::setConnection($conn);
+//$createUser = Users::register("1@1", "123", "123", "To jest uzytkownik 1@! lalala", "Janusz111");
 //var_dump($createUser);
-//$login = Users::login("apf1234@goo.pl", "haslo1");
+//$login = Users::login("root1", "coderslab");
 //$login->setDescription("lalalatu");
 //$login->saveToDB();
+//$login->getUserName();
 //var_dump($login);
+
+//$allOutbox = Message::viewAllOutboxByAuthorId(7);
+//var_dump($allOutbox);
+
+//$mess = Message::getReciverInfo(10);
+//var_dump($mess);
+
+//$mess = Message::getReciverName(10);
+//echo ($mess);
+
+//$newMessage1 = Message::createMessage(8,10, "Hi Janusz");
+//var_dump($newMessage1);
+
+//var_dump($login);
+//$createComment = Comment::createComment(1,1,"To komentarz user-1 twee-1");
+//var_dump($createComment);
+
+/*
+$gettweetById = Tweet::getTweetById(1);
+foreach($gettweetById as $val) {
+    echo "<br>". $val->getText();
+    echo "date: {$val->getCreationDate()}";
+}
+
+$getComByTweetId = Comment::getCommentByTweetId(1);
+foreach($getComByTweetId as $val){
+    echo $val->getText();
+    echo "date: {$val->getCreationDate()}";
+
+}
+*/
+
+//$myNewTweet = Tweet::createTweet(1, "to jest pierwszy tweet");
+//var_dump($myNewTweet);
